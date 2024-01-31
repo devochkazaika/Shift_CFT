0034c5796425909827e71729f14eeb5d2465a60c
Panarin Ilya <cccthafcdh@gmail.com>
2024-01-31 13:25:56 +0700

    sadasd

---
+++ build/resources/main/db.migration/V1__create_tables.sql
@@ -0,0 +1,16 @@
+--DROP TABLE users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    walletid VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    fullname VARCHAR(255),
+    email VARCHAR(255),
+    phone INT,
+    registrationdate DATE,
+    lastupdatedate DATE,
+    age INT
+);
+
+--create table if not exists check_create_table(id bigserial not null);
